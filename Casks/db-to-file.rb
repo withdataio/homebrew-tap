b253b04e14273513ca1f74f557cd98854f4934d6
pan <panshiji@163.com>
2026-09-20 22:59:08 -0700

    Update db-to-file to 3.0.3

--- Casks/db-to-file.rb
+++ Casks/db-to-file.rb
@@ -1,6 +1,6 @@
 cask "db-to-file" do
-  version "3.0.2"
-  sha256 "f8030af64d6d821d147e50b310006a06db7b994f41e929720418fd72c71cc6a4"
+  version "3.0.3"
+  sha256 "7dde25cb49212713655805ea875c4f0cdb401af0d269cc28f67b777339e550f7"
 
   url "https://www.withdata.com/down/DBToFile-#{version}.pkg"
   name "DBToFile"
