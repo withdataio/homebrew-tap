770b5575d003126ce1e9983d34972f5431e477ac
pan <panshiji@163.com>
2026-09-20 20:21:02 -0700

    Update mongo-to-file to 4.8.1

--- Casks/mongo-to-file.rb
+++ Casks/mongo-to-file.rb
@@ -1,6 +1,6 @@
 cask "mongo-to-file" do
-  version "4.7.1"
-  sha256 "7731ae5e33dfd93090820cdd8c13e52bdf1ad6eba87f9cc09faae67dc9ffb9f5"
+  version "4.8.1"
+  sha256 "e4b074fcd404081e46e9f6f8851f10cb9ba36380b9d2922dd1a09e4a7d8a1c43"
 
   url "https://www.withdata.com/down/MongoToFile-#{version}.pkg"
   name "MongoToFile"
