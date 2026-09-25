4c74b41758044782b2bf7480abb2aa4e7d359b46
pan <panshiji@163.com>
2026-09-24 20:47:57 -0700

    Update mongo-loader to 2.9.3

--- Casks/mongo-loader.rb
+++ Casks/mongo-loader.rb
@@ -1,6 +1,6 @@
 cask "mongo-loader" do
-  version "2.9.2"
-  sha256 "a65aebe5a6c133c63378a14ee96b517131968583d20f72fae6d5d59ea10d9cd7"
+  version "2.9.3"
+  sha256 "5710d6f20f71d47e69cd2d1f447a0626fd540792ff04ce80ee9b0d339b6b2cb1"
 
   url "https://www.withdata.com/down/MongoLoader-#{version}.pkg"
   name "MongoLoader"
